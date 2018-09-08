1f34224eed5afcfb7b482589421fbb0c4ee0fe50
Oscar Knagg <oscar@knagg.co.uk>
2018-09-08 10:29:22 +0100

    See above

--- utils.py
+++ utils.py
@@ -15,3 +15,29 @@ def whiten(batch, rms=0.038021):
     whitened_batch = whitened_batch * np.tile(sample_wise_rescaling, (1, 1, batch.shape[1])).transpose((1, 2, 0))
 
     return whitened_batch
+
+
+def evaluate_siamese_network(siamese, dataset, preprocessor, num_tasks, n, k):
+    """Evaluate a siamese network on k-way, n-shot classification tasks generated from a particular dataset."""
+    # Currently assumes 1 shot classification in evaluation loop
+    if n != 1:
+        raise NotImplementedError
+
+    # TODO: Faster/multiprocessing creation of tasks
+    n_correct = 0
+    for i_eval in range(num_tasks):
+        query_sample, support_set_samples = dataset.build_n_shot_task(k, n)
+
+        input_1 = np.stack([query_sample[0]] * k)[:, :, np.newaxis]
+        input_2 = support_set_samples[0][:, :, np.newaxis]
+
+        # Perform preprocessing
+        input_1, input_2 = preprocessor((input_1, input_2))
+
+        pred = siamese.predict([input_1, input_2])
+
+        if np.argmin(pred[:, 0]) == 0:
+            # 0 is the correct result as by the function definition
+            n_correct += 1
+
+    return n_correct
