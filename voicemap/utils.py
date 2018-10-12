1bfa3a211b17eb518f3601f8f771e3c93d221d76
Oscar Knagg <oscar@knagg.co.uk>
2018-10-12 18:58:12 +0100

    Updated whiten follow pytorch axis conventions

--- voicemap/utils.py
+++ voicemap/utils.py
@@ -5,17 +5,17 @@ def whiten(batch, rms=0.038021):
     """This function whitens a batch of samples so each sample
     has 0 mean and the same root mean square amplitude i.e. volume.
 
-    NB This function operates on a 3D torch.Tensor of shape (n_samples, n_timesteps, 1)
+    NB This function operates on a 3D torch.Tensor of shape (n_samples, 1, n_timesteps,)
     """
     if len(batch.shape) != 3:
-        raise(ValueError, 'Input must be a 3D array of shape (n_samples, n_timesteps, 1).')
+        raise(ValueError, 'Input must be a 3D array of shape (n_samples, 1, n_timesteps,).')
 
     # Subtract mean
-    sample_wise_mean = batch.mean(dim=1)
-    whitened_batch = batch - sample_wise_mean.repeat([batch.shape[1], 1, 1]).transpose(dim0=1, dim1=0)
+    sample_wise_mean = batch.mean(dim=2)
+    whitened_batch = batch - sample_wise_mean.repeat([1, 1, batch.shape[2]]).transpose(dim0=1, dim1=0)
 
     # Divide through
-    rescaling_factor = rms / torch.sqrt(torch.mul(batch, batch).mean(dim=1))
-    whitened_batch = whitened_batch*rescaling_factor.repeat([batch.shape[1], 1, 1]).transpose(dim0=1, dim1=0)
+    rescaling_factor = rms / torch.sqrt(torch.mul(batch, batch).mean(dim=2))
+    whitened_batch = whitened_batch*rescaling_factor.repeat([1, 1, batch.shape[2]]).transpose(dim0=1, dim1=0)
 
     return whitened_batch
