e5810605ab90af9ccd10254eaef7022cea3ddc4f
Oscar Knagg <oscar@knagg.co.uk>
2018-10-20 18:43:15 +0100

    Added Omniglot dataset for few-shot learning experiments

--- voicemap/datasets.py
+++ voicemap/datasets.py
@@ -1,4 +1,5 @@
 from torch.utils.data import Dataset
+from skimage import io
 from tqdm import tqdm
 import soundfile as sf
 import pandas as pd
@@ -305,3 +306,77 @@ class LibriSpeechDataset(Dataset):
 
         progress_bar.close()
         return audio_files
+
+
+class OmngiglotDataset(Dataset):
+    def __init__(self, subset):
+        if subset not in ('background', 'evaluation'):
+            raise(ValueError, 'subset must be one of (background, evaluation)')
+        self.subset = subset
+
+        self.df = pd.DataFrame(self.index_subset(self.subset))
+
+        # Index of dataframe has direct correspondence to item in dataset
+        self.df = self.df.assign(id=self.df.index.values)
+
+        # Convert arbitrary class names of dataset to ordered 0-(num_speakers - 1) integers
+        self.unique_characters = sorted(self.df['class_name'].unique())
+        self.class_name_to_id = {self.unique_characters[i]: i for i in range(self.num_classes())}
+        self.df = self.df.assign(class_id=self.df['class_name'].apply(lambda c: self.class_name_to_id[c]))
+
+        # Create dicts
+        self.datasetid_to_filepath = self.df.to_dict()['filepath']
+        self.datasetid_to_class_id = self.df.to_dict()['class_id']
+
+    def __getitem__(self, item):
+        instance = io.imread(self.datasetid_to_filepath[item])
+        # Reindex to channels first format as supported by pytorch
+        instance = instance[np.newaxis, : , :]
+
+        # Normalise to 0-1
+        instance = (instance - instance.min()) / (instance.max() - instance.min())
+
+        label = self.datasetid_to_class_id[item]
+
+        return instance, label
+
+    def __len__(self):
+        return len(self.df)
+
+    def num_classes(self):
+        return len(self.df['class_name'].unique())
+
+    @staticmethod
+    def index_subset(subset):
+        """
+        Index a subset by looping through all of it's files and recording their speaker ID, filepath and length.
+        :param subset: Name of the subset
+        :return: A list of dicts containing information about all the audio files in a particular subset of the
+        LibriSpeech dataset
+        """
+        images = []
+        print('Indexing {}...'.format(subset))
+        # Quick first pass to find total for tqdm bar
+        subset_len = 0
+        for root, folders, files in os.walk(PATH + '/data/Omniglot/images_{}/'.format(subset)):
+            subset_len += len([f for f in files if f.endswith('.png')])
+
+        progress_bar = tqdm(total=subset_len)
+        for root, folders, files in os.walk(PATH + '/data/Omniglot/images_{}/'.format(subset)):
+            if len(files) == 0:
+                continue
+
+            alphabet = root.split('/')[-2]
+            class_name = '{}.{}'.format(alphabet, root.split('/')[-1])
+
+            for f in files:
+                progress_bar.update(1)
+                images.append({
+                    'subset': subset,
+                    'alphabet': alphabet,
+                    'class_name': class_name,
+                    'filepath': os.path.join(root, f)
+                })
+
+        progress_bar.close()
+        return images
