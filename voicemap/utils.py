1c64ff0c00689c96353035fa378ea960f1011056
Oscar Knagg <oscar@knagg.co.uk>
2018-10-21 17:49:51 +0100

    Added pairwise distance utils function

--- voicemap/utils.py
+++ voicemap/utils.py
@@ -19,3 +19,23 @@ def whiten(batch, rms=0.038021):
     whitened_batch = whitened_batch*rescaling_factor.repeat([1, 1, batch.shape[2]]).transpose(dim0=1, dim1=0)
 
     return whitened_batch
+
+
+def pairwise_distances(x, y=None):
+    """
+    Input: x is a Nxd matrix
+           y is an optional Mxd matirx
+    Output: dist is a NxM matrix where dist[i,j] is the square norm between x[i,:] and y[j,:]
+            if y is not given then use 'y=x'.
+    i.e. dist[i,j] = ||x[i,:]-y[j,:]||
+    """
+    x_norm = (x**2).sum(1).view(-1, 1)
+    if y is not None:
+        y_norm = (y**2).sum(1).view(1, -1)
+    else:
+        y = x
+        y_norm = x_norm.view(1, -1)
+
+    dist = x_norm + y_norm - 2.0 * torch.mm(x, torch.transpose(y, 0, 1))
+
+    return torch.sqrt(dist)
