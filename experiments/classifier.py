1fc37f455163e370b51b6dc6af11ad59ced1931a
Oscar Knagg <oscar@knagg.co.uk>
2019-04-27 19:07:47 +0100

    Cleanup of classifier training script

--- experiments/classifier.py
+++ experiments/classifier.py
@@ -1,4 +1,5 @@
 import torch
+from torch import optim, nn
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from multiprocessing import cpu_count
@@ -6,8 +7,8 @@ import argparse
 from olympic.callbacks import CSVLogger, Evaluate, ReduceLROnPlateau, ModelCheckpoint
 from olympic import fit
 
-from voicemap.datasets import LibriSpeech
-from voicemap.models import ResidualClassifier
+from voicemap.datasets import LibriSpeech, SpeakersInTheWild, ClassConcatDataset
+from voicemap.models import ResidualClassifier, get_classifier
 from voicemap.utils import whiten, setup_dirs
 from config import PATH
 
@@ -22,45 +23,41 @@ torch.backends.cudnn.benchmark = True
 # Parameters #
 ##############
 parser = argparse.ArgumentParser()
+parser.add_argument('--lr', type=float)
+parser.add_argument('--l2', type=float)
+parser.add_argument('--momentum', type=float)
+parser.add_argument('--epochs', type=int)
 parser.add_argument('--filters', type=int)
-parser.add_argument('--embedding-dim', type=int)
 parser.add_argument('--batch-size', type=int)
 parser.add_argument('--n-seconds', type=int)
 parser.add_argument('--downsampling', type=int)
 args = parser.parse_args()
 
-param_str = f'filters={args.filters}__embedding_dim={args.embedding_dim}__n_seconds={args.n_seconds}__' \
-    f'downsampling={args.downsampling}'
+param_str = '__'.join([f'{k}={str(v)}' for k, v in vars(args).items()])
+
 print(param_str)
 
 test_fraction = 0.1
-# n-shot task params
-num_tasks = 500
-n_shot = 1
-k_way = 5
 
 
 ###################
 # Create datasets #
 ###################
-# dataset = ['train-clean-100', 'train-clean-360', 'train-other-500']
-dataset = ['dev-clean', ]
-data = LibriSpeech(dataset, args.n_seconds, args.downsampling, stochastic=True, pad=False)
+# librispeech_subsets = ['dev-clean', 'train-clean-100', 'train-clean-360', 'train-other-500']
+librispeech_subsets = ['dev-clean', ]
+librispeech = LibriSpeech(librispeech_subsets, args.n_seconds, args.downsampling, stochastic=True, pad=False)
+sitw = SpeakersInTheWild('enroll-core', args.n_seconds, args.downsampling, True, True, '/home/oscar/data/sitw')
+
+data = ClassConcatDataset([librispeech, sitw])
+num_classes = data.num_classes
+print(f'Total no. speakers = {num_classes}')
 
 unseen_speakers = LibriSpeech('dev-clean', args.n_seconds, args.downsampling, stochastic=True, pad=False)
 
 indices = range(len(data))
-speaker_ids = data.df['speaker_id'].values
+speaker_ids = librispeech.df['speaker_id'].values.tolist() + sitw.df['speaker_id'].values.tolist()
 train_indices, test_indices, _, _ = train_test_split(indices, speaker_ids, test_size=test_fraction,
                                                      stratify=speaker_ids)
-
-gb = data.df.iloc[train_indices].groupby('speaker_id').agg({'seconds': 'sum'})
-print('TRAIN: {} unique speakers with {:.1f}+-{:.1f} seconds of audio each,'.format(
-    len(gb), gb['seconds'].mean(), gb['seconds'].std()))
-
-gb = data.df.iloc[test_indices].groupby('speaker_id').agg({'seconds': 'sum'})
-print('TEST: {} unique speakers with {:.1f}+-{:.1f} seconds of audio each,'.format(
-    len(gb), gb['seconds'].mean(), gb['seconds'].std()))
 
 train = torch.utils.data.Subset(data, train_indices)
 test = torch.utils.data.Subset(data, test_indices)
@@ -69,7 +66,8 @@ test = torch.utils.data.Subset(data, test_indices)
 ################
 # Define model #
 ################
-model = ResidualClassifier(args.filters, [2, 2, 2], data.num_classes())
+# model = ResidualClassifier(args.filters, [2, 2, 2, 2], num_classes)
+model = get_classifier(args.filters, 128, num_classes)
 model.to(device, dtype=torch.double)
 
 
@@ -78,8 +76,8 @@ model.to(device, dtype=torch.double)
 ############
 train_loader = DataLoader(train, batch_size=args.batch_size, num_workers=cpu_count(), shuffle=True, drop_last=True)
 test_loader = DataLoader(test, batch_size=args.batch_size, num_workers=cpu_count(), shuffle=True, drop_last=True)
-opt = torch.optim.SGD(model.parameters(), lr=0.05, momentum=0.9)
-loss_fn = torch.nn.CrossEntropyLoss().cuda()
+opt = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.l2)
+loss_fn = nn.CrossEntropyLoss().cuda()
 
 
 def prepare_batch(batch):
@@ -97,24 +95,17 @@ def prepare_n_shot_batch(query, support):
 
 callbacks = [
     Evaluate(test_loader),
-    # Evaluate n-shot tasks on seen classes
-    # NShotTaskEvaluation(num_tasks=num_tasks, n_shot=n_shot, k_way=k_way, dataset=data_stochastic,
-    #                     prepare_batch=prepare_n_shot_batch, prefix=''),
-    # # Evaluate n-shot on tasks on unseen classes
-    # NShotTaskEvaluation(num_tasks=num_tasks, n_shot=n_shot, k_way=k_way, dataset=unseen_speakers,
-    #                     prepare_batch=prepare_n_shot_batch, prefix='test_'),
     ReduceLROnPlateau(monitor='val_accuracy', patience=5, verbose=True, min_delta=0.005),
     ModelCheckpoint(filepath=PATH + f'/models/classifier_{param_str}.pt',
-                    monitor='val_categorical_accuracy'),
+                    monitor='val_accuracy', save_best_only=True, verbose=True),
     CSVLogger(PATH + f'/logs/classifier_{param_str}.csv'),
 ]
-
 
 fit(
     model,
     opt,
     loss_fn,
-    epochs=50,
+    epochs=args.epochs,
     dataloader=train_loader,
     prepare_batch=prepare_batch,
     callbacks=callbacks,
